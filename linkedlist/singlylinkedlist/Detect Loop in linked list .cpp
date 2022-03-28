b474667c0b42f5310e5d74da2f4ca53e9acb8aea
Bittuday <69690713+Bittuday@users.noreply.github.com>
2022-03-28 22:28:24 +0530

    Create Detect Loop in linked list .cpp

---
+++ linkedlist/singlylinkedlist/Detect Loop in linked list .cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {       
+        if(!head)
+        return false;
+            Node* low=head;
+            Node* high=head;
+            while(high!=NULL&&high->next!=NULL)
+            {
+                low=low->next;
+                high=high->next->next;
+                if(low==high)
+                {
+                    return true;
+                }
+                
+            }
+                return false;
+    }
+    
+};
